b6455110c590800cd0e329046d3494a141965c15
AnasNAK <112939383+AnasNAK@users.noreply.github.com>
2022-09-07 17:34:57 +0100

    Add files via upload

--- exemple1.c
+++ exemple1.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
-int date_naissance,X,age;
-int main(){
-    printf("donner votre date de naissance?\n");
-    scanf("%d",&date_naissance);
-    printf("qulle annee sommes nous maintenant?\n");
-    scanf("%d",&X);
-    age = X - date_naissance;
-    printf("est voilla vous aurez %d ans cette annee !!",age);
+
+int main (){
+    int a;
+     float b;
+
+     printf("entrer votre N compte de 6 nombre :");
+     scanf("%d",&a);
+     if(!(a>=100000&&a<=999999))
+     printf("numero non valide ");
+   else
+    printf("bnjour de votre compte\n");
+    printf("ajouter votre montant DH :");
+scanf("%f",&b);
+   if (!(b>100&&b<100))
+    printf("e minium c est 100DH.Merci pour resseyez pus tard");
 
 }
